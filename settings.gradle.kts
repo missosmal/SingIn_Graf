e81f638c8d049115fbd540195d7071f5ab859581
VladPyankov1337 <vladipyankov@yandex.ru>
2024-04-06 10:38:14 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SingIn_Graf"
+include(":app")
+ 
