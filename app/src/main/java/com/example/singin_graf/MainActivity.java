b81a9e9bdbfb254f8e8d0d79cb3bc9f85ef9f0c1
missosmal <fadesteam8@gmail.com>
2024-04-13 09:38:22 +0500

    Разработанный код для переключения страниц.Graf

--- app/src/main/java/com/example/singin_graf/MainActivity.java
+++ app/src/main/java/com/example/singin_graf/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.singin_graf;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.MotionEvent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +9,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signin);
+    }
+
+    public int start_x = 0;
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch (event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                start_x = (int)event.getX();
+                break;
+            case  MotionEvent.ACTION_UP:
+                if(Math.abs((int)event.getX() - start_x) > 50)
+                {
+                    if(start_x < (int)event.getX())
+                    {
+                        setContentView((R.layout.signin));
+                    } else {
+                        setContentView(R.layout.regin);
+                    }
+                }
+        }
+        return false;
     }
 }
