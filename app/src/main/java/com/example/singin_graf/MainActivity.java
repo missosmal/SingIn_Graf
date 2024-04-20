c6a838f305d0d110fad10d1368351abf9ca2ebd7
missosmal <fadesteam8@gmail.com>
2024-04-20 09:38:40 +0500

    Создание класса DataUser.Graf

--- app/src/main/java/com/example/singin_graf/MainActivity.java
+++ app/src/main/java/com/example/singin_graf/MainActivity.java
@@ -3,6 +3,20 @@ package com.example.singin_graf;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,4 +48,31 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
+    public String login, password;
+
+    public void onAuthorization(View view)
+    {
+        TextView tv_login = findViewById(R.id.editTextTextPersonNane);
+        login = tv_login.getText().toString();
+
+        TextView tv_password = findViewById(R.id.editTextTextPersonName2);
+        password = tv_password.getText().toString();
+
+        GetDataUser gdu = new GetDataUser();
+        gdu.execute();
+    }
+    public class DataUser{
+        public String id;
+        public String login;
+        public String password;
+
+        public void setId(String _id) {this.id = _id;}
+        public String getId(){return this.id;}
+        public void setLogin(String _login) {this.login = _login;}
+        public String getLogin(){
+            return this.login;}
+        public void setPassword(String _password) {this.password = _password;}
+        public String getPassword(){return this.password;}
+    }
+    ArrayList<DataUser> dataUser = new ArrayList();
 }
