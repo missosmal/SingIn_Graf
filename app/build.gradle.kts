440f32b7463e538b38ac3f79232e96b1ac24148e
missosmal <fadesteam8@gmail.com>
2024-04-20 09:12:02 +0500

    Установка библиотеки.Graf

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,9 @@
+import org.gradle.internal.impldep.bsh.commands.dir
+
+
+
+
+
 plugins {
     id("com.android.application")
 }
@@ -32,11 +38,12 @@ android {
 }
 
 dependencies {
-
+    implementation ("org.jsoup:jsoup:1.17.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
